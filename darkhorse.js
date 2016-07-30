35c6d979f55386fdca693103e0c2aa3223851ea9
louissankey <lmsankey@ucdavis.edu>
2016-07-30 15:13:15 -0600

    even better

--- darkhorse.js
+++ darkhorse.js
@@ -1,18 +1,18 @@
 /*jslint node: true */
 'use strict';
 
-// var express = require('express');
-// var app     = express();
+var express = require('express');
+var app     = express();
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-// //For avoidong Heroku $PORT error
-// app.get('/', function(request, response) {
-//     var result = 'App is running';
-//     response.send(result);
-// }).listen(app.get('port'), function() {
-//     console.log('App is running, server is listening on port ', app.get('port'));
-// });
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running';
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 var firebase = require("./node_modules/firebase");
 //will be used for scheduling jobs
