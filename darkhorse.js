7bc561f09023d84de86c5310721c61c06235a34a
louissankey <lmsankey@ucdavis.edu>
2016-07-30 14:24:00 -0600

    even better

--- darkhorse.js
+++ darkhorse.js
@@ -1,11 +1,11 @@
 /*jslint node: true */
 'use strict';
 
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(process.env.PORT, '0.0.0.0');
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(process.env.PORT, '0.0.0.0');
 
 
 
