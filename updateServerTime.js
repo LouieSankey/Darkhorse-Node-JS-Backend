f7451c2a9b7720bff03c27b115c26785e5cf9a78
louissankey <lmsankey@ucdavis.edu>
2016-08-18 19:54:54 -0600

    added a value for updating the server time to firebase at midnight

---
+++ updateServerTime.js
@@ -0,0 +1,24 @@
+function update(){
+
+var http = require('http');
+var moment =require('moment');
+var firebase = require("./node_modules/firebase");
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(process.env.PORT, '0.0.0.0');
+
+firebase.initializeApp({
+  serviceAccount: "serviceAccountCredentials.json",
+  databaseURL: "https://darkhorsefantasysports.firebaseio.com/"
+});
+
+
+var firebaseDb = firebase.database();
+var serverTime = firebaseDb.ref('serverTime');
+
+serverTime.set(Firebase.ServerValue.TIMESTAMP);
+
+}
+update();
