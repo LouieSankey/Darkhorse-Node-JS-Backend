ac01bddf2856b7676fa69ab2cf193c11488f2075
louissankey <lmsankey@ucdavis.edu>
2016-10-20 10:53:32 -0700

    contests

--- initializeContests.js
+++ initializeContests.js
@@ -221,8 +221,6 @@ schedule.child(formattedScheduleDate).on('value', function (snapshot){
 						contestsRef.child(formattedScheduleDate).on('child_added', function(contestSnapshot){
 
 							var contest = contestSnapshot.val();
-							console.log("listening to contst: " + contest);
-
 							var accepting = contest.accepting;
 							var draftEnds = contest.draftEnds;
 							var entryAmnt = contest.entryAmnt;
@@ -238,7 +236,221 @@ schedule.child(formattedScheduleDate).on('value', function (snapshot){
 							var contestEntryRef = contestsRef.child(formattedScheduleDate).child(contestSnapshot.key).child("Entries");
 
 	
-								contestEntryRef.on('child_added', function(entrySnapshot){
+								//new code block for initializing players
+								var playersInContest = [];
+								contestEntryRef.on('child_added', function(newEntry){
+
+                        playersInContest.push({
+                               "playerKey": newEntry.key,
+                               "name": newEntry.val().name,
+                               "playerId": newEntry.val().playerId
+
+                        });
+
+
+                  var vsRef = entries.child(newEntry.key).child("VS");
+
+//for new entries, adds a default "scores" object with a path to each entry already in the contest
+
+
+                  for (var i = 0; i < playersInContest.length; i++) {
+
+
+                         if(playersInContest[i].playerKey != newEntry.key){
+
+
+                              vsRef.child(playersInContest[i].playerKey).update({
+
+                                    
+                                    "0": {
+                                          "matchSet": false,
+                                          "opponentName": playersInContest[i].name,
+                                          "playerBuy": "0",
+                                          "playerName": newEntry.val().name,
+                                          "statCategory": "Pts",
+                                          "playerId": playersInContest[i].playerId,
+                                          "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "1": {
+                                          "matchSet": false,
+                                          "opponentName": playersInContest[i].name,
+                                          "playerBuy": "0",
+                                          "playerName": newEntry.val().name,
+                                          "statCategory": "Reb",
+                                          "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "2": {
+                                          "matchSet": false,
+                                          "opponentName": playersInContest[i].name,
+                                          "playerBuy": "0",
+                                          "playerName": newEntry.val().name,
+                                          "statCategory": "Ast",
+                                          "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "3": {
+                                          "matchSet": false,
+                                          "opponentName": playersInContest[i].name,
+                                          "playerBuy": "0",
+                                          "playerName": newEntry.val().name,
+                                          "statCategory": "Stl",
+                                          "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "4": {
+                                          "matchSet": false,
+                                          "opponentName": playersInContest[i].name,
+                                          "playerBuy": "0",
+                                          "playerName": newEntry.val().name,
+                                          "statCategory": "Blk",
+                                          "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "5": {
+                                          "matchSet": false,
+                                          "opponentName": playersInContest[i].name,
+                                          "playerBuy": "0",
+                                          "playerName": newEntry.val().name,
+                                          "statCategory": "3Pt",
+                                          "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "6": {
+                                          "matchSet": false,
+                                          "opponentName": playersInContest[i].name,
+                                          "playerBuy": "0",
+                                          "playerName": newEntry.val().name,
+                                          "statCategory": "Low TO",
+                                          "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    }
+                                    
+                                    
+                              });
+
+
+                              var oppVsRef = entries.child(playersInContest[i].playerKey).child("VS");
+
+
+//for entries already in the contest, adds a "scores" object for the new entry. 
+//also need to notify entries already in the contest of the new entry
+
+                              console.log("notification " + playersInContest[i].playerKey);
+
+                                // request({
+                                //   url: 'https://fcm.googleapis.com/fcm/send',
+                                //   method: 'POST',
+                                //   headers: {
+                                //     'Content-Type' :' application/json',
+                                //     'Authorization': 'key=AIzaSyDgYtB8klH4KbDgeml3YmzpAnhb2_m6Y8s'
+                                //   },
+                                //   body: JSON.stringify({
+                                //     data: {
+                                //       message: "A player has drafted into your contest."
+                                //     },
+                                //     to : '/topics/user_'+ playersInContest[i].playerKey
+                                //   })
+                                // });
+
+
+                                // console.log('/topics/user_'+ playersInContest[i].playerKey);
+
+                                // sendMessageToUser(
+                                //   '/topics/user_'+ playersInContest[i].playerKey,
+                                //   { message: "A player has drafted into your contest."}
+                                // );
+
+
+
+                              //sendNotificationToUser(playersInContest[i].playerKey, "a player has drafted into your contest");
+
+
+                               oppVsRef.child(newEntry.key).update({
+
+                                    "0": {
+
+                                          "matchSet": false,
+                                          "opponentName": newEntry.val().name,
+                                          "playerBuy": "0",
+                                          "playerName": playersInContest[i].name,
+                                          "statCategory": "Pts",
+                                          "playerId": newEntry.val().playerId,
+                                           "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "1": {
+                                          "matchSet": false,
+                                          "opponentName": newEntry.val().name,
+                                          "playerBuy": "0",
+                                          "playerName": playersInContest[i].name,
+                                          "statCategory": "Reb",
+                                           "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "2": {
+                                          "matchSet": false,
+                                          "opponentName": newEntry.val().name,
+                                          "playerBuy": "0",
+                                          "playerName": playersInContest[i].name,
+                                          "statCategory": "Ast",
+                                           "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "3": {
+                                          "matchSet": false,
+                                          "opponentName": newEntry.val().name,
+                                          "playerBuy": "0",
+                                          "playerName": playersInContest[i].name,
+                                          "statCategory": "Stl",
+                                           "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "4": {
+                                          "matchSet": false,
+                                          "opponentName": newEntry.val().name,
+                                          "playerBuy": "0",
+                                          "playerName": playersInContest[i].name,
+                                          "statCategory": "Blk",
+                                           "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "5": {
+                                          "matchSet": false,
+                                          "opponentName": newEntry.val().name,
+                                          "playerBuy": "0",
+                                          "playerName": playersInContest[i].name,
+                                          "statCategory": "3Pt",
+                                           "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    },
+                                    "6": {
+                                          "matchSet": false,
+                                          "opponentName": newEntry.val().name,
+                                          "playerBuy": "0",
+                                          "playerName": playersInContest[i].name,
+                                          "statCategory": "Low TO",
+                                           "plusAmount": 0,
+                                          "opponentPlus": 0
+                                    }
+                                    
+                                    });
+
+
+                                  }
+
+
+                  }
+
+    }, function (error) {
+
+    });
+
+
+
+
+
 
 									var playersEntered;
 									contestEntryRef.once("value", function(children) {
@@ -283,7 +495,7 @@ schedule.child(formattedScheduleDate).on('value', function (snapshot){
 
 	
 
-});
+// });
 
 
 
