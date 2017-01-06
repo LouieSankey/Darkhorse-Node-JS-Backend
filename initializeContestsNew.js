1c77bff362656e8e0d5f93fd530c3fef5d7f861e
louissankey <lmsankey@ucdavis.edu>
2017-01-05 17:41:30 -0800

    darkhorse

--- initializeContestsNew.js
+++ initializeContestsNew.js
@@ -1,18 +1,18 @@
-function update(){
+function update(firebase){
 
 var http = require('http');
 var moment =require('moment');
 var firebase = require("./node_modules/firebase");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(process.env.PORT, '0.0.0.0');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(process.env.PORT, '0.0.0.0');
 
-firebase.initializeApp({
-  serviceAccount: "serviceAccountCredentials.json",
-  databaseURL: "https://darkhorsefantasysports.firebaseio.com/"
-});
+// firebase.initializeApp({
+//   serviceAccount: "serviceAccountCredentials.json",
+//   databaseURL: "https://darkhorsefantasysports.firebaseio.com/"
+// });
 
 
 var firebaseDb = firebase.database();
